d0c7f821cbdb983aaa168f38b951f3efa12b33a4
yhoi <ddmmmktennis@gmail.com>
2023-04-14 09:12:19 +0900

    feature/Headerの追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import Header from '../components/Header'
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Header />
       <Component {...pageProps} />
     </ChakraProvider>
   )
